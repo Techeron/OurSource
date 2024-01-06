32ce46d0a83856c7d524e66d9e663c3f6c071479
codokk101 <codokk101@gmail.com>
2024-01-06 01:56:47 -0500

    Event Registration

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "node:url";
 import {
   Client,
   Collection,
+  Events,
   GatewayIntentBits,
   REST,
   Routes,
@@ -21,7 +22,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 const commandBody = [];
-
+// Command Registration
 for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder);
   const commandFiles = fs
@@ -41,7 +42,35 @@ for (const folder of commandFolders) {
     }
   }
 }
+// Command Execution
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
+// Login
 client.login(process.env.DISCORD_TOKEN).then(() => {
   // Guilds
   client.guilds.fetch().then((guilds) => {
